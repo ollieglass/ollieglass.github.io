b34cfc783a6e4aca3f51e89b7852d28eb14048d6
Ollie Glass <ollie@ollieglass.com>
2018-06-20 10:26:25 +0100

    new site

---
+++ component---src-pages-404-js-d025bce1c0dd4fc4545e.js
@@ -0,0 +1,2 @@
+webpackJsonp([0x9427c64ab85d],{200:function(e,t,u){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}t.__esModule=!0;var l=u(4),a=n(l),d=u(65),f=n(d),o=function(){return a.default.createElement("div",null,a.default.createElement("h1",null,"Page not found"),a.default.createElement(f.default,{to:"/"},"Return home"))};t.default=o,e.exports=t.default}});
+//# sourceMappingURL=component---src-pages-404-js-d025bce1c0dd4fc4545e.js.map
