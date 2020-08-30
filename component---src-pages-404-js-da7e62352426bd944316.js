f1a8e577de4e618285726a899e226fa8b9d9dd7a
Ollie Glass <ollie@ollieglass.com>
2020-08-30 11:32:20 +0100

    update

---
+++ component---src-pages-404-js-da7e62352426bd944316.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{w2l6:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),l=t.n(a),o=t("Wbzz"),u=t("Bl7J");n.default=function(){return l.a.createElement(u.a,null,l.a.createElement("h1",null,"Page not found"),l.a.createElement(o.a,{to:"/"},"Return home"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-da7e62352426bd944316.js.map
